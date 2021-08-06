6973707f1a82a738b5bde5dfe1ef0eac6f61404c
DerVerruckte <michael.heinhold@gmail.com>
2021-08-06 09:27:39 -0600

    added prompts for length and uppercase

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -3,17 +3,34 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
+  var length = (parseInt(userLength()));
+    function userLength(){
+      prompt("How many characters long do you need your password?")
+        if(userLength<=128){
+          return userLength
+        }
+        else{
+          return 128
+        }
+    }
+
+  var charset = (userChar() + "abcdefghijklmnopqrstuvwxyz")
+    function userChar() { 
+      confirm("Do you need any upercase letters? If yes click 'Ok.'")
+      if(userChar === true){
+        return("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+      }
+      else{
+        return("")
+      }
+    }
   
   
-  
-  
-  var length = 8,
-      charset = "abcdefghijklmnopqrstuvwxyz",
-      password="";
+  var password="";
   for (var i =0, n =charset.length; i <length; ++i) {
     password += charset.charAt(Math.floor(Math.random()*n));
   }
-  
+  debugger
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
