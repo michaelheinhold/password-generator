d2e9ada86d042ef1aa8802681adf7b11aa316499
DerVerruckte <michael.heinhold@gmail.com>
2021-08-10 12:21:42 -0600

    created functions for length, up, num,and specials

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,36 +1,116 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var lowChar = "abcdefghijklmnopqrstuvwxyz";
+var upChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var numChar = "0123456789";
+var specialChar="!@#$%^&*()_+=-";
+var length;
+var checkUpChar;
+var checkNumChar;
+var checkSpecialChar;
+
+//determines lenght of password
+function defineLength(){
+  var length = prompt("Choose a length between 8-128 characters for your password.")
+
+  if(length < 8) {
+    alert("Please choose a length between 8-128 characters.");
+    defineLength();
+  } else if (length>128) {
+    alert("Please choose a length between 8-128 characters.");
+    defineLength();
+  } else if (isNaN(length)) {
+    alert("Please choose a length between 8-128 characters.");
+    defineLength();
+  }else {
+    alert("Now you will be asked if you need uppercase letters, numbers, or any special characters. If you select no on all, you will be given a password with only lowercase letters.")
+    return length;
+  }
+  return length;
+}
+
+//determines if uppercase is needed
+function determineUpChar() {
+  var checkUpChar = prompt("Do you need Uppercase letters? Respond 'Yes' or 'No'");
+    checkUpChar=checkUpChar.toLowerCase();
+  
+  if(checkUpChar === null || checkUpChar === "") {
+    alert("Please respond with 'Yes' or 'No'");
+    
+    determineUpChar();
+    
+  }else if (checkUpChar === 'yes' || checkUpChar === 'y'){
+    checkUpChar === true;
+    return checkUpChar;
+  }else if (checkUpChar === 'no' || checkUpChar === 'n'){
+    checkUpChar === false;
+    return checkUpChar;
+  }else {
+    alert("Please respond with 'Yes' or 'No'");
+    determineUpChar();
+  }
+  return checkUpChar;
+}
+
+// determins is numbers are needed
+function determineNumChar() {
+  var checkNumChar = prompt("Do you need Numbers? Respond 'Yes' or 'No'");
+    checkNumChar= checkNumChar.toLowerCase();
+
+  if(checkNumChar === null || checkNumChar === "") {
+    alert("Please respond with 'Yes' or 'No'");
+    
+    determineNumChar();
+    
+  }else if (checkNumChar === 'yes' || checkNumChar === 'y'){
+    checkNumChar === true;
+    return checkNumChar;
+  }else if (checkNumChar === 'no' || checkNumChar === 'n'){
+    checkNumChar === false;
+    return checkNumChar;
+  }else {
+    alert("Please respond with 'Yes' or 'No'");
+    determineNumChar();
+  }
+  return checkNumChar;
+}
+
+//determines if special characters are needed
+function determineSpecialChar() {
+  var checkSpecialChar = prompt("Do you need Special Characters? Respond 'Yes' or 'No'");
+    checkSpecialChar=checkSpecialChar.toLowerCase();
+  
+  if(checkSpecialChar === null || checkSpecialChar === "") {
+    alert("Please respond with 'Yes' or 'No'");
+    
+    determineSpecialChar();
+    
+  }else if (checkSpecialChar === 'yes' || checkSpecialChar === 'y'){
+    checkSpecialChar === true;
+    return checkSpecialChar;
+  }else if (checkSpecialChar === 'no' || checkSpecialChar === 'n'){
+    checkSpecialChar === false;
+    return checkSpecialChar;
+  }else {
+    alert("Please respond with 'Yes' or 'No'");
+    determineSpecialChar();
+  }
+  return checkSpecialChar;
+}
+
+//record entries
+defineLength();
+console.log(length);
+determineUpChar();
+console.log(checkUpChar);
+determineNumChar();
+console.log(checkNumChar);
+determineSpecialChar();
+console.log(checkSpecialChar);
+
 
 // Write password to the #password input
 function writePassword() {
-  var length = (parseInt(userLength()));
-    function userLength(){
-      prompt("How many characters long do you need your password?")
-        if(userLength<=128){
-          return userLength
-        }
-        else{
-          return 128
-        }
-    }
-
-  var charset = (userChar() + "abcdefghijklmnopqrstuvwxyz")
-    function userChar() { 
-      confirm("Do you need any upercase letters? If yes click 'Ok.'")
-      if(userChar === true){
-        return("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
-      }
-      else{
-        return("")
-      }
-    }
-  
-  
-  var password="";
-  for (var i =0, n =charset.length; i <length; ++i) {
-    password += charset.charAt(Math.floor(Math.random()*n));
-  }
-  debugger
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
---
+++ psuedo-code.md
@@ -0,0 +1,17 @@
+-prompts to ask for password criteria after password buttn is pressed
+    asks for uppercase, numbers, and specials (default if none selected is all lowercase)
+    create and store variables for each criteria input
+    place each variable in an array
+-length of password needs to be 8-128 characters
+    use a seperate function to determine
+    check using if statements for 8-128 range
+-use seperate functions to determine and store user input
+    for every prompt check against falsy inputs
+-have a random number generator pick values in array for the length specified
+-var for length, lowercase, uppercase, numbers, and specials
+-function to generate the password called when button is clicked
+-a value to be placed in the text field in the html document
+
+
+
+
